8d6f55f79dc8c4d42ed16bd1e7836fb223f7555a
SchorchedVenom <43762540+SchorchedVenom@users.noreply.github.com>
2024-02-23 23:37:25 -0500

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-// displayCookies.js
-window.onload = function() {
-    if (document.cookie) {
-        alert("Cookies: " + document.cookie);
-    } else {
-        alert("No cookies found.");
-    }
-};
-
